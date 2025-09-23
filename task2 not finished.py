ed048aa58f46d2ccd22b1e0478eb6eb4974d7d19
lev <lew.znnz@mail.ru>
2025-09-23 23:01:06 +0400

    Update task2.py

--- task2 not finished.py
+++ task2 not finished.py
@@ -12,7 +12,7 @@ def tableEquation(a, b, step ):
     for i in range(start_int, end_int, step_int):
         x = i / 100.0
         y = (x ** (1 / (2 * x))) + ((x ** 2) * math.log((x ** 3))) / ((math.sin(x) ** 2) + (math.cos(x) ** 2))
-        print("x:", x, "y:", round(y, 5)) # idk how to write it smooth
+        print("x:", x, "y:", round(y, 5)) # IDK how to write it smooth
 
 tableEquation(0.1, 1.5,0.05)
 
@@ -23,7 +23,7 @@ tableEquation(0.1, 1.5,0.05)
 def tableEquationUser():
     try:
         a = float(input("input a start of segment (a): "))
-        b = float(input("input a start of segment (b): "))
+        b = float(input("input a end of segment (b): "))
         step = float(input("input a step: "))
 
 
