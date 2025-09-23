b9acb8e96a39c6a514866fa9bdf3d63cabac9818
lev <lew.znnz@mail.ru>
2025-09-23 22:58:04 +0400

    Update task2.py

--- task2 not finished.py
+++ task2 not finished.py
@@ -1,6 +1,6 @@
 import math
 
-""" SecondTask: """
+""" SecondTask, work with func """
 
 
 def tableEquation(a, b, step ):
@@ -12,6 +12,31 @@ def tableEquation(a, b, step ):
     for i in range(start_int, end_int, step_int):
         x = i / 100.0
         y = (x ** (1 / (2 * x))) + ((x ** 2) * math.log((x ** 3))) / ((math.sin(x) ** 2) + (math.cos(x) ** 2))
-        print("x: ", x)
+        print("x:", x, "y:", round(y, 5)) # idk how to write it smooth
 
 tableEquation(0.1, 1.5,0.05)
+
+
+""" SecondTask, interaction with user: """
+
+
+def tableEquationUser():
+    try:
+        a = float(input("input a start of segment (a): "))
+        b = float(input("input a start of segment (b): "))
+        step = float(input("input a step: "))
+
+
+        start_int = int(a * 100)
+        end_int = int(b * 100)
+        step_int = int(step * 100)
+
+        for i in range(start_int, end_int, step_int):
+            x = i / 100.0
+            y = (x ** (1 / (2 * x))) + ((x ** 2) * math.log((x ** 3))) / ((math.sin(x) ** 2) + (math.cos(x) ** 2))
+            print("x:", x, "y:", round(y, 5)) # too
+
+    except ValueError:
+        return "wrong index"
+
+print(tableEquationUser())
