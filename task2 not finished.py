08731f422db713ff924918da822223ac6b620f8b
lev <lew.znnz@mail.ru>
2025-09-23 22:15:41 +0400

    Update task2 almost finished.py

--- task2 not finished.py
+++ task2 not finished.py
@@ -2,10 +2,16 @@ import math
 
 """ SecondTask: """
 
-a = 15
-b = 1
-step = 0
 
+def tableEquation(a, b, step ):
 
-for x in range(b, a, step):
-    y = (x ** (1 / (2 * x))) + ((x ** 2) * math.log((x ** 3))) / ((math.sin(x) ** 2) + (math.cos(x) ** 2))
+    start_int = int(a * 100)
+    end_int = int(b * 100)
+    step_int = int(step * 100)
+
+    for i in range(start_int, end_int, step_int):
+        x = i / 100.0
+        y = (x ** (1 / (2 * x))) + ((x ** 2) * math.log((x ** 3))) / ((math.sin(x) ** 2) + (math.cos(x) ** 2))
+        print("x: ", x)
+
+tableEquation(0.1, 1.5,0.05)
