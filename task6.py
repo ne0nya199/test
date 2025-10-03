c11b5e705163041b981f50bdad1213dfee656d7e
lev <lew.znnz@mail.ru>
2025-10-03 20:34:49 +0400

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,23 @@
+def checkModuleOfFirstElm():
+    try:
+        countOfNums = int(input("enter count of numbers: "))
+        arrayNums = []
+        equalElementsInArray = -1
+
+        for userInteraction in range(countOfNums):
+            numsOfUser = int(input())
+            arrayNums.append(numsOfUser)
+
+        print("numbers in array:", arrayNums[countOfNums - 1])
+
+        for x in range(countOfNums):
+            if abs(arrayNums[0]) == abs(arrayNums[0 + (x - 1)]):
+                x += 1
+                equalElementsInArray += 1
+        print("number of elements with equal modules (with first value): ", equalElementsInArray)
+
+    except ValueError:
+        print("Please, enter a number")
+
+
+checkModuleOfFirstElm()
