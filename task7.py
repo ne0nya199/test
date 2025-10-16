2c02077e55cb465cd3a19625bfddfb6d3dd54361
lev <lew.znnz@mail.ru>
2025-10-16 23:06:27 +0400

    Update task7.py

--- task7.py
+++ task7.py
@@ -11,26 +11,35 @@ def checkModule():
         checkedElement = 1
 
         for b in range(countOfNums):
-            if arrayNums[takenNum] < 0:
+            if arrayNums[takenNum] < 0 and takenNum < countOfNums - 1:
                 takenNum += 1
-                checkedElement += 1
-
+                if checkedElement < countOfNums - 1:
+                    checkedElement += 1
             else:
                 if arrayNums[checkedElement] > 0:
                     if checkedElement < countOfNums - 1:
                         checkedElement += 1
                 else:
-                    for x in range(takenNum, countOfNums - takenNum):
+                    for x in range(countOfNums):
                         if abs(arrayNums[takenNum]) < abs(arrayNums[checkedElement]):
                             print(f"module of element {takenNum}: ", arrayNums[takenNum], f", lesser than module of {checkedElement}: ", arrayNums[checkedElement])
                             if checkedElement != countOfNums - 1:
                                 checkedElement += 1
-                                break
+                                while arrayNums[checkedElement] > 0 and checkedElement < countOfNums - 1:
+                                    checkedElement += 1
                             else:
-                                takenNum += 1
-                                for nextNum in range(takenNum, countOfNums - 1):
-                                    if arrayNums[takenNum] < 0:
+                                if takenNum != countOfNums - 1:
+                                    takenNum += 1
+                                    while arrayNums[takenNum] < 0 and takenNum < countOfNums - 1:
                                         takenNum += 1
+                                    if checkedElement != countOfNums - 1 or takenNum != countOfNums - 1:
+                                        checkedElement = takenNum + 1
+                                    else:
+                                        break
+                                else:
+                                    break
+
+
     except ValueError:
         print("Please, enter a number")
 
