6b27aa2bea5f5954b219f807c701f8c537650b1a
lev <lew.znnz@mail.ru>
2025-10-17 14:00:37 +0400

    Update task8.py

--- task8.py
+++ task8.py
@@ -1,15 +1,17 @@
 flashDrive = ["110 mb"]
-strLaptop = [15, 17, 18, 19 , 100]
+strLaptop = [150, 17, 18, 19 , 100]
 userLaptop = []
 
 def copyFiles():
     int_b = int(flashDrive[0].replace(' mb', ''))
-    initialValue = int_b
+    initialValue = int_b; indexOfnum = 0
     for x in range(len(strLaptop)):
-        if int_b >= strLaptop[0]:
-            flashDrive.append(strLaptop[0])
-            int_b -= strLaptop[0]
-            strLaptop.remove(strLaptop[0])
+        if int_b >= strLaptop[indexOfnum]:
+            flashDrive.append(strLaptop[indexOfnum])
+            int_b -= strLaptop[indexOfnum]
+            strLaptop.remove(strLaptop[indexOfnum])
+        else:
+            indexOfnum += 1
     flashDrive[0] = flashDrive[0].replace(str(initialValue), str(int_b))
     print("u have: ", flashDrive[0], "| ur folders:", flashDrive[1: len(flashDrive)], "| remaining folders on pc: ", strLaptop)
 
